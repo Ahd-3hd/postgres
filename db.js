62302b76ea852dbc728b60ac8a4ce1f4c488833f
ahd <ahdhani0@gmail.com>
2020-09-22 07:33:58 +0300

    setup postgres and server

---
+++ db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: 5432,
+  database: process.env.DB,
+});
+
+module.exports = pool;
