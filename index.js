122ef3592aea36f00cb9cacbece992b54b302fdf
ahd <ahdhani0@gmail.com>
2020-09-22 07:47:20 +0300

    basic crud operations

--- index.js
+++ index.js
@@ -35,8 +35,48 @@ app.get("/todos", async (req, res) => {
 });
 // get a todo
 
+app.get("/todos/:id", async (req, res) => {
+  try {
+    console.log(req.params);
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 //update a todo
 
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    console.log(updateTodo);
+    res.json("todo was updated");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 //delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("todo was deleted bro");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.listen(5000, () => console.log("server started on 5000"));
